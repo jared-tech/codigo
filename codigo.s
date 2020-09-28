c5ea911e536d3aa14415779d5000dce53afdca33
jared-tech <71996034+jared-tech@users.noreply.github.com>
2020-09-27 21:11:32 -0500

    Add files via upload

---
+++ codigo.s
@@ -0,0 +1,40 @@
+PROCESSOR 16F887
+#include <xc.inc>
+;CONFIG word1
+CONFIG FOSC = INTRC_NOCLKOUT
+CONFIG WDTE = OFF
+CONFIG PWRTE = ON
+CONFIG MCLRE = OFF
+CONFIG CP = OFF
+CONFIG CPD = OFF
+CONFIG BOREN = OFF
+CONFIG IESO = OFF
+CONFIG FCMEN = ON
+CONFIG DEBUG = ON
+;CONFIG word2
+CONFIG BOR4V=BOR40V
+CONFIG WRT = OFF
+PSECT udata
+ mask:
+    DS 1
+cont:
+    DS 1
+var3:
+    DS 1
+
+PSECT resetVec,class=CODE,delta=2
+resetVec:
+PAGESEL main
+goto main
+PSECT code
+ main:
+ bcf 0x03,6
+ bcf 0x03,5
+ movlw 0b10010010
+ ANDLW 0b00100111
+ movwf mask
+ goto main
+ END
+
+
+
